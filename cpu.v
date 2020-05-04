efe559d12b946e1f5895b4b18159d07c9120d454
Varun Gorti <vgorti@cs.utexas.edu>
2020-05-03 21:21:06 -0500

    Basic pause/resume working

--- cpu.v
+++ cpu.v
@@ -14,11 +14,11 @@ wire halt1;
 wire halt2;
 
 wire[16:0] pc_passed_1 = 17'h0;
-wire[16:0] pc_passed_2 = 17'h0;
+wire[16:0] pc_passed_2 = 17'h200;
 
 wire[2:0] stall_num_1;
 assign stall_num_1 = pauseResume_1 === 3'b100 | pauseResume_2 === 3'b100 ? 6 :
-	pauseResume[1] === 0 & pauseResume_1 !== 3'b110 & pauseResume_2 !== 3'b110 ? 6 :
+	pauseResume[0] === 0 & pauseResume_1 !== 3'b110 & pauseResume_2 !== 3'b110 ? 6 :
 	0;
 wire[2:0] stall_num_2;
 assign stall_num_2 = pauseResume_1 === 3'b101 | pauseResume_2 === 3'b101 ? 6 :
